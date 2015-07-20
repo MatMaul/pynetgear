6fccc0b573c4a0cf5a32d3d673da1d8fcfadf50f
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-07-19 21:31:00 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pynetgear',
+      version='0.1',
+      description='Access Netgear routers using their SOAP API',
+      url='http://github.com/balloob/pynetgear',
+      author='Paulus Schoutsen',
+      author_email='Paulus@PaulusSchoutsen.nl',
+      license='MIT',
+      install_requires=['requests>=2.0'],
+      packages=['pynetgear'],
+      zip_safe=True)
