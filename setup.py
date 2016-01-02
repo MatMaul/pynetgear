dd01473bd3c8d972d4109a93240e725e27ff6360
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-01-02 10:24:36 -0800

    Version bump to 0.3.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynetgear',
-      version='0.3',
+      version='0.3.1',
       description='Access Netgear routers using their SOAP API',
       url='http://github.com/balloob/pynetgear',
       author='Paulus Schoutsen',
