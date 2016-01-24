12be06d6d5d5d7cf473323ad0912cfdce8f56af9
digdugg <barndt@gmail.com>
2016-01-24 10:16:22 -0700

    reformatted to use if/elif syntax versus nesting for readability

--- pynetgear/__init__.py
+++ pynetgear/__init__.py
@@ -74,10 +74,10 @@ class Netgear(object):
                 # The last device, ignore the last element
                 info = data[start:-1]
 
-            if len(info) < 4:
-                if len(info) == 0:
-                    # don't warn on null lists just continue
-                    continue
+
+            if len(info) == 0:
+                continue
+            elif len(info) < 4:
                 _LOGGER.warning('Unexpected entry: %s', info)
                 continue
 
