f74ee2e2eff754c38d74c40966b6ac70ec89580a
Mathieu Velten <matmaul@gmail.com>
2018-10-02 00:11:41 +0200

    Filter out non valid XML characters

--- pynetgear/__init__.py
+++ pynetgear/__init__.py
@@ -6,8 +6,33 @@ from collections import namedtuple
 import logging
 import xml.etree.ElementTree as ET
 from datetime import timedelta
+import re
+import sys
 
 import requests
+
+
+# define regex to filter invalid XML codes
+# cf https://stackoverflow.com/questions/1707890/fast-way-to-filter-illegal-xml-unicode-chars-in-python
+if sys.version_info[0] == 3:
+    unichr = chr
+_illegal_unichrs = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F),
+                        (0x7F, 0x84), (0x86, 0x9F),
+                        (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)]
+if sys.maxunicode >= 0x10000:  # not narrow build
+    _illegal_unichrs.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
+                             (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
+                             (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+                             (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
+                             (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
+                             (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+                             (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
+                             (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)])
+
+_illegal_ranges = ["%s-%s" % (unichr(low), unichr(high))
+                   for (low, high) in _illegal_unichrs]
+_illegal_xml_chars_RE = re.compile(u'[%s]' % u''.join(_illegal_ranges))
+
 
 DEFAULT_HOST = 'routerlogin.net'
 DEFAULT_USER = 'admin'
@@ -287,6 +312,7 @@ def autodetect_url():
 
 
 def _find_node(response, xpath):
+    response = _illegal_xml_chars_RE.sub('', response)
     it = ET.iterparse(StringIO(response))
     # strip all namespaces
     for _, el in it:
