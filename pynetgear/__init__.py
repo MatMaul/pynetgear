92138199d37d1214cbed84afdbbcd5bbc5b941b3
Quentin POLLET <polletquentin74@me.com>
2020-04-29 23:30:16 +0200

    Autodetect URL with timeout + test non HTTPS first

--- pynetgear/__init__.py
+++ pynetgear/__init__.py
@@ -408,13 +408,20 @@ def autodetect_url():
 
     Returns None if it can't be found.
     """
-    for url in ["http://routerlogin.net:5000", "https://routerlogin.net",
-                "http://routerlogin.net"]:
+    DETECTABLE_URLS = [
+        "http://routerlogin.net",
+        "http://routerlogin.net:5000",
+        "https://routerlogin.net",
+    ]
+    for url in DETECTABLE_URLS:
         try:
-            r = requests.get(url + "/soap/server_sa/",
-                             headers=_get_soap_headers("Test:1", "test"),
-                             verify=False)
-            if r.status_code == 200:
+            resp = requests.get(
+                url + "/soap/server_sa/",
+                headers=_get_soap_headers("Test:1", "test"),
+                verify=False,
+                timeout=1
+            )
+            if resp.status_code == 200:
                 return url
         except requests.exceptions.RequestException:
             pass
