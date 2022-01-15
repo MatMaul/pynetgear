14c9647d8855f3b234e1ffef40b44eb2821b4f77
starkillerOG <starkiller.og@gmail.com>
2022-01-15 18:37:58 +0100

    add pypi discription

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from codecs import open
+import pathlib
 
 REPO_URL = "http://github.com/MatMaul/pynetgear"
 VERSION = "0.8.0"
@@ -7,10 +8,18 @@ VERSION = "0.8.0"
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
     name="pynetgear",
     version=VERSION,
     description="Access Netgear routers using their SOAP API",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url=REPO_URL,
     download_url=REPO_URL + "/tarball/" + VERSION,
     author="Paulus Schoutsen",
