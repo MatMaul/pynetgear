3d4b3977df7d4c1bcfaa6c556cdc336a8c3bf000
starkillerOG <starkiller.og@gmail.com>
2022-01-31 10:25:18 +0100

    fix backwords compatibility

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 from codecs import open
-import pathlib
 
 REPO_URL = "http://github.com/MatMaul/pynetgear"
 VERSION = "0.8.0"
@@ -8,11 +7,8 @@ VERSION = "0.8.0"
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+with open("README.md") as f:
+    README = f.read()
 
 setup(
     name="pynetgear",
