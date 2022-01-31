4db06351aff56b7eafa3a44357c38caec0e0375d
starkillerOG <starkiller.og@gmail.com>
2022-01-31 10:28:24 +0100

    add pypi discription (#103)

--- setup.py
+++ setup.py
@@ -7,10 +7,15 @@ VERSION = "0.9.0"
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
+with open("README.md") as f:
+    README = f.read()
+
 setup(
     name="pynetgear",
     version=VERSION,
     description="Access Netgear routers using their SOAP API",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url=REPO_URL,
     download_url=REPO_URL + "/tarball/" + VERSION,
     author="Paulus Schoutsen",
