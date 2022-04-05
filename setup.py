4d008355f4fcb515fba658a4e8ed3bcefcd437b4
starkillerOG <starkiller.og@gmail.com>
2022-04-05 14:17:00 +0200

    bump to version 0.9.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from codecs import open
 
 REPO_URL = "http://github.com/MatMaul/pynetgear"
-VERSION = "0.9.3"
+VERSION = "0.9.4"
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
