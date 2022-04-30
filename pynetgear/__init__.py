44992425a921fd24f093a7c3bb19c7ad4c1c595d
starkillerOG <starkiller.og@gmail.com>
2022-04-30 17:43:03 +0200

    fix init import erros

--- pynetgear/__init__.py
+++ pynetgear/__init__.py
@@ -1,4 +1,5 @@
 """Module to communicate with Netgear routers using the SOAP v2 API."""
+# flake8: noqa
 
 from .router import Device, Netgear
 from .const import ALLOW, BLOCK, DEFAULT_HOST, DEFAULT_USER, DEFAULT_PORT
